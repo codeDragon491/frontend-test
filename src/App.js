ba3a3bfe06326bf9cc62a78dc39b78fdb9c9af74
JULIA TREFAS <trefasjulia@gmail.com>
2020-10-05 15:37:05 +0200

    add router and create documents container component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.scss";
+import Documents from "./documents";
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Redirect from="/" to="/browse-legal-documents" exact />{" "}
+        <Route path="/browse-legal-documents" component={Documents} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/documents/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./documents.module.scss";
+
+class Documents extends React.Component {
+  render() {
+    return <div className={styles.page}></div>;
+  }
+}
+
+export default Documents;
