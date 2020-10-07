b9e2e3c562c36d3ee4498abeda5fe3cc7ab0c292
JULIA TREFAS <trefasjulia@gmail.com>
2020-10-07 08:19:19 +0200

    create svg background

--- src/documents/index.js
+++ src/documents/index.js
@@ -3,8 +3,27 @@ import styles from "./documents.module.scss";
 
 class Documents extends React.Component {
   render() {
-    return <div className={styles.page}></div>;
+    return (
+      <div className={styles.page}>
+        <div className={styles.background}>
+          <svg
+            width="100%"
+            height="100%"
+            viewBox="0 0 1204 1154"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M-195.962 -167.085C-300.087 -166.928 -366.124 -62.163 -315.764 22.9957L314.109 1088.06C365.984 1175.79 500.89 1175.58 553.064 1087.7L1186.55 20.7149C1237.2 -64.59 1171.53 -169.156 1067.39 -169L-195.962 -167.085Z"
+              fill="#004F54"
+              fillOpacity="0.05"
+            />
+          </svg>
+        </div>
+      </div>
+    );
   }
 }
-
 export default Documents;
